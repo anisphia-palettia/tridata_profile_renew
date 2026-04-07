ddb3c5e386f7bfb9a4c9b1af751b2d048a38c812
anisphia-palettia <euphyllia.tomodachi@gmail.com>
2026-04-08 05:38:04 +0700

    feat: configure basePath and assetPrefix for speedtest

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  basePath : "/speedtest",
+  assetPrefix : "/speedtest",
 };
 
 export default nextConfig;
